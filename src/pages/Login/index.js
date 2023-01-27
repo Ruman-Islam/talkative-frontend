018f1f4f2c7d3fe3660cc8236b304a6579ad5a92
Azharul Islam <lonewolfazharul@gmail.com>
2023-01-27 22:12:23 +0600

    added login & registration

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,13 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import Head from "next/head";
+import { useForm } from "react-hook-form";
+import Link from "next/link";
 
 const Index = () => {
+
+  const { register, formState: { errors }, handleSubmit } = useForm();
+  const [loginError, setLoginError] = useState('');
+  const [loginUserEmail, setLoginUserEmail] = useState('');
+
+  const handleLogin = data => {
+
+    console.log(data.email, data.password);
+  }
+
   return (
     <>
       <Head>
-        <title>Register</title>
+        <title>Login</title>
       </Head>
-      <div>this is login</div>
+      <div className="h-[100vh] hero min-h-screen">
+        <div className="hero-content flex-col lg:flex-row">
+          <div className="text-center lg:text-left">
+            <img src='https://i.ibb.co/JQP6jCp/3-Q0k3-RC-CWEAMUZb-Q-Eu-N-transformed.jpg' alt='login' width={500} height={500} />
+          </div>
+          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <div className='flex justify-center items-center'>
+              <div className='w-96 p-7'>
+                <h2 className='font-extrabold text-neutral text-2xl text-center'>Login</h2>
+                <form onSubmit={handleSubmit(handleLogin)}>
+                  <div className="text-neutral form-control w-full max-w-xs">
+                    <label className="label"> <span className="label-text text-neutral">Email</span></label>
+                    <input type="text"
+                      {...register("email", {
+                        required: "Email Address is required"
+                      })}
+                      className="input input-bordered w-full max-w-xs" />
+                    {errors.email && <p className='text-red-600'>{errors.email?.message}</p>}
+                  </div>
+                  <div className="form-control w-full max-w-xs">
+                    <label className="label"> <span className="label-text text-neutral">Password</span></label>
+                    <input type="password"
+                      {...register("password", {
+                        required: "Password is required",
+                        minLength: { value: 6, message: 'Password must be 6 characters or longer' }
+                      })}
+                      className="text-neutral input input-bordered w-full max-w-xs" />
+                    {errors.password && <p className='text-red-600'>{errors.password?.message}</p>}
+                    <label className="label"> <span className="label-text text-neutral"></span></label>
+                  </div>
+                  <input className='btn btn-primary w-full' value="Login" type="submit" />
+                  <div>
+                    {loginError && <p className='text-red-600'>{loginError}</p>}
+                  </div>
+                </form>
+                <p className="text-neutral">New to Talkative? <Link href='/Register/Index' className='font-bold text-info'>Create New Account</Link></p>
+                <div className="divider"> </div>
+                <button className='btn btn-primary btn-outline w-full'>Google Login</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,13 +1,91 @@
 import Head from "next/head";
-import React from "react";
+import Link from "next/link";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 
 const Index = () => {
+
+  const { register, formState: { errors }, handleSubmit } = useForm();
+  const [signupError, setSignupError] = useState('');
+  const [createdUserEmail, setCreatedUserEmail] = useState('');
+
+  const handleRegister = data => {
+    console.log(data);
+  }
+
   return (
     <>
       <Head>
         <title>Register</title>
       </Head>
-      <div>this is register</div>
+      <div className="h-[800px] hero min-h-screen">
+        <div className="hero-content flex-col lg:flex-row">
+          <div className="text-center lg:text-left">
+            <img src='https://i.ibb.co/pQq98Qw/online-registration-sign-up-with-man-sitting-near-smartphone-268404-95.webp' alt='login' width={500} height={500} />
+          </div>
+          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <div className='flex justify-center items-center'>
+              <div className='w-96 p-7'>
+                <h2 className='font-extrabold text-neutral text-2xl text-center'>Register</h2>
+                <form onSubmit={handleSubmit(handleRegister)}>
+                  <div className="text-neutral form-control w-full max-w-xs">
+                    <label className="label"> <span className="label-text text-neutral">User Name</span></label>
+                    <input type="text" placeholder="User name" {...register("User name", { required: true, maxLength: 80 })}
+                      className="input input-bordered w-full max-w-xs" />
+                  </div>
+                  <div className="text-neutral form-control w-full max-w-xs">
+                    <label className="label"> <span className="label-text text-neutral">Email</span></label>
+                    <input type="text" placeholder="Enter email"
+                      {...register("email", {
+                        required: "Email Address is required"
+                      })}
+                      className="input input-bordered w-full max-w-xs" />
+                    {errors.email && <p className='text-red-600'>{errors.email?.message}</p>}
+                  </div>
+                  <div className="form-control w-full max-w-xs">
+                    <label className="label"> <span className="label-text text-neutral">Password</span></label>
+                    <input type="password" placeholder="Enter password"
+                      {...register("password", {
+                        required: "Password is required",
+                        minLength: { value: 6, message: 'Password must be 6 characters or longer' }
+                      })}
+                      className="text-neutral input input-bordered w-full max-w-xs" />
+                    {errors.password && <p className='text-red-600'>{errors.password?.message}</p>}
+                    <label className="label"> <span className="label-text text-neutral"></span></label>
+                  </div>
+                  <div className="text-neutral form-control w-full max-w-xs">
+                    <label className="label"> <span className="label-text text-neutral">Country</span></label>
+                    <input type="text" placeholder="Enter country name" {...register("Country", { required: true, maxLength: 80 })} className="input input-bordered w-full max-w-xs" />
+                  </div>
+                  <div className="text-neutral form-control w-full max-w-xs">
+                    <label className="label"> <span className="label-text text-neutral">Age</span></label>
+                    <input type="number" placeholder="Enter age" {...register("Age", { required: true, max: 59, min: 17, maxLength: 101 })} className="input input-bordered w-full max-w-xs" />
+                  </div>
+                  <div className="text-neutral form-control w-full max-w-xs">
+                    <label className="label"> <span className="label-text text-neutral">Birthday</span></label>
+                    <input type="date" placeholder="Enter birthday" {...register("Birthday", { required: true, maxLength: 12 })} className="input input-bordered w-full max-w-xs" />
+                  </div>
+                  <div className="text-neutral form-control w-full max-w-xs">
+                    <label className="label"> <span className="label-text text-neutral">Skill</span></label>
+                    <input type="number" placeholder="Add skill level" {...register("Skill", { max: 10, min: 0 })} className="input input-bordered w-full max-w-xs" />
+                  </div>
+                  <div className="text-neutral form-control w-full max-w-xs">
+                    <label className="label"> <span className="label-text text-neutral">Native Language</span></label>
+                    <input type="text" placeholder="Native Language" {...register("Enter language", { maxLength: 14 })} className="input input-bordered w-full max-w-xs" />
+                  </div>
+                  <input className='btn btn-primary w-full' value="Register" type="submit" />
+                  <div>
+                    {signupError && <p className='text-red-600'>{signupError}</p>}
+                  </div>
+                </form>
+                <p className="text-neutral">Already have an account? <Link href='/Login/Index' className='font-bold text-info'>login</Link></p>
+                <div className="divider"> </div>
+                <button className='btn btn-primary btn-outline w-full'>Google Signup</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
